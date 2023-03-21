63b061a01ecc31fc3eb646253d30f1005c424b03
андрей <p.aleksey2010@yandex.ru>
2023-03-21 20:07:37 +0300

    .\Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Menu m = new Menu();
+        Operation o = new Operation(new MainLogger());
+
+        List<Option> operationsList = new ArrayList<>();
+        operationsList.add(new SumRational());
+        operationsList.add(new SumComplex());
+        operationsList.add(new MinusRational());
+        operationsList.add(new MinusComplex());
+        operationsList.add(new MultRational());
+        operationsList.add(new MultComplex());
+        operationsList.add(new DivRational());
+        operationsList.add(new DivComplex());
+
+        boolean flag = true;
+        while (flag){
+            System.out.println("Выберите номер действия:");
+            for (int i = 0; i < operationsList.size(); i++) {
+                System.out.println(i+" - "+operationsList.get(i).discription());
+            }
+            int chek = input.nextInt();
+            operationsList.get(chek).execute();
+
+            System.out.println("Хотите продолжить? Нажмите 1 если да, 2 - нет");
+            int in = input.nextInt();
+            if (in == 2) flag = false;
+
+        }
+    }
+}
