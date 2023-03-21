8d33a73989c2668eecf9425be647241ed9898227
андрей <p.aleksey2010@yandex.ru>
2023-03-21 20:52:08 +0300

    .\Operation.java

---
+++ Operation.java
@@ -0,0 +1,13 @@
+public class Operation {
+    Logirable logger;
+    MainLogger ml = new MainLogger();
+    public Operation(Logirable logger){
+        this.logger = logger;
+    }
+    public Operation(){ }
+    public void  getAnswer(String str){
+        System.out.println(str);
+        ml.doLog(new Logger(str));
+    }
+
+    }
